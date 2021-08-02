090cceb589e34e83e86dfbd29761043eeae986ce
Anton Beleshnikov <antonbeleshnikov@yandex.ru>
2021-08-02 03:24:13 +0300

    1-е задание

--- core/src/ru/gb/StarGame.java
+++ core/src/ru/gb/StarGame.java
@@ -1,6 +1,7 @@
 package ru.gb;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -8,17 +9,20 @@ import com.badlogic.gdx.utils.ScreenUtils;
 public class StarGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	Texture background;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		background = new Texture("space.jpg");
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
+		batch.draw(background, 0,0, Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
 		batch.draw(img, 0, 0);
 		batch.end();
 	}
@@ -27,5 +31,6 @@ public class StarGame extends ApplicationAdapter {
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
+		background.dispose();
 	}
 }
