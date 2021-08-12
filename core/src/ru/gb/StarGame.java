7e73799284cd77be895eea5cb09159edc924b742
Anton Beleshnikov <antonbeleshnikov@yandex.ru>
2021-08-12 12:24:10 +0300

    Апдейт проекта

--- core/src/ru/gb/StarGame.java
+++ core/src/ru/gb/StarGame.java
@@ -5,9 +5,10 @@ import com.badlogic.gdx.Game;
 import ru.gb.screen.MenuScreen;
 
 public class StarGame extends Game {
-	
+
 	@Override
 	public void create () {
-		setScreen(new MenuScreen());
+		setScreen(new MenuScreen(this));
 	}
+
 }
